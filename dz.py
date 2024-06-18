4bd48c95457f37e24d4100b2fd81e9a9028edc5e
Anna_Krivosheeva <130230527+AnnaWolfgang@users.noreply.github.com>
2024-06-18 14:09:09 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -15,3 +15,16 @@ data.columns = data.columns.droplevel()
 data.columns.name = None
 data.head()
 
+
+
+\\или
+\\import pandas as pd
+\\import random
+\\lst = ['robot'] * 10
+\\lst += ['human'] * 10
+\\random.shuffle(lst)
+\\data = pd.DataFrame({'whoAmI': lst})
+\\one_hot_data = pd.DataFrame()
+\\for value in data['whoAmI'].unique():
+\\    one_hot_data[value] = data['whoAmI'].apply(lambda x: 1 if x == value else 0)
+\\print(one_hot_data.head())
