d271c29c3a0bfd7b4d1db42815b8cc65fe74f0a8
Anna_Krivosheeva <130230527+AnnaWolfgang@users.noreply.github.com>
2024-06-18 14:00:54 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -6,7 +6,12 @@ lst += ['human'] * 10
 random.shuffle(lst)
 
 data = pd.DataFrame({'whoAmI': lst})
+data.head()
 
-one_hot_encoded = pd.get_dummies(data, columns=['whoAmI'])
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+data.head()
 
-print(one_hot_encoded.head())
