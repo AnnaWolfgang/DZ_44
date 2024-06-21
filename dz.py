2540fb902254bb13674edc9f0725163700ea9b6d
Anna_Krivosheeva <130230527+AnnaWolfgang@users.noreply.github.com>
2024-06-21 13:17:56 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -17,14 +17,14 @@ data.head()
 
 
 
-\\или
-\\import pandas as pd
-\\import random
-\\lst = ['robot'] * 10
-\\lst += ['human'] * 10
-\\random.shuffle(lst)
-\\data = pd.DataFrame({'whoAmI': lst})
-\\one_hot_data = pd.DataFrame()
-\\for value in data['whoAmI'].unique():
-\\    one_hot_data[value] = data['whoAmI'].apply(lambda x: 1 if x == value else 0)
-\\print(one_hot_data.head())
+//или
+//import pandas as pd
+//import random
+//lst = ['robot'] * 10
+//lst += ['human'] * 10
+//random.shuffle(lst)
+//data = pd.DataFrame({'whoAmI': lst})
+//one_hot_data = pd.DataFrame()
+//for value in data['whoAmI'].unique():
+//    one_hot_data[value] = data['whoAmI'].apply(lambda x: 1 if x == value else 0)
+//print(one_hot_data.head())
